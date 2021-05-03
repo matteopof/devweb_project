ae1b75c5511a731f8948a306a188f825146ae954
DAGO Kokri Esaïe <dago.esaie@protonmail.com>
2021-05-03 09:23:06 +0200

    random vote increment

--- src/components/vote/index.jsx
+++ src/components/vote/index.jsx
@@ -1,56 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const createOption = name => ({ name : name, votes : 0 });
 
-const testOptions = [
+const votesArray = [
   createOption('A'),
   createOption('B#m'),
   createOption('Csus2'),
   createOption('B/F#')
-]
+];
 
-export class VoteZone extends React.Component {
+const voteFor = (choise) => {
+  votesArray[choise].votes += 1;
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {choords : [...testOptions]}
-  }
+const winner = () => {
+  return votesArray.reduce(
+    (prev, curr) => (curr.votes > prev.votes ? curr : prev)
+  )
+};
 
-  voteFor(choise) {
-    let tmp = [...this.state.choords];
-    tmp[choise].votes += 1;
-    this.setState({choords : tmp});
-  }
+export const VoteZone = () => {
 
-  winner() {
-    return this.state.choords.reduce(
-      (prev, curr) => (curr.votes > prev.votes ? curr : prev)
-    )
-  }
+  const [seconds, setSeconds] = useState(0);
 
-  render() {
-    return (
-      <>
-        <div className="container">
-        <h2>Choord Chooser 2000</h2>
-          {
-            this.state.choords.map((option, i) => 
-              <div key={i} className="voteOption">
-                <div className="chordName">
-                  {option.name}
-                </div>
-                <div className="voteCount">
-                  {option.votes}
-                </div>
-                <button onClick={this.voteFor.bind(this, i)}>+1</button>
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+        let ran = Math.floor(Math.random() * 4)
+          console.log(ran);
+          voteFor(ran);
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+
+  return (
+    <>
+      <div className="container">
+      <h2>Choord Chooser 2000</h2>
+        {
+          votesArray.map((option, i) => 
+            <div key={i} className="voteOption">
+              <div className="chordName">
+                {option.name}
               </div>
-            )
-          }
-          <div className="voteWinner">
-            And the winner is : {this.winner().name}
-          </div>
+              <div className="voteCount">
+                {option.votes}
+              </div>
+            </div>
+          )
+        }
+        <div className="voteWinner">
+          And the winner is : {winner().name}
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 };
