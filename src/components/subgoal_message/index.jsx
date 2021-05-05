0daa391f16f9c65664dcc1fb53022d613bfa9235
Fanny <fannyjoseph@gmail.com>
2021-05-05 12:21:39 +0200

    tests

--- src/components/subgoal_message/index.jsx
+++ src/components/subgoal_message/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState }  from 'react';
 import subGoal from '../sub_bar/index';
 import axios from 'axios';
-
 var chat= [];
 axios.get ( "https://api.npoint.io/8fbad75c668cb9509ea2")
 .then (res => chat = res.data)
@@ -9,27 +8,27 @@ axios.get ( "https://api.npoint.io/8fbad75c668cb9509ea2")
 
 export const SubgoalMessage = () => {
     const [time, setTime] = useState(0);
-    const [subs, setSub] = useState(38);
+    const [subs, setSub] = useState(0);
 
     const [messagedisplay, setMessageDisplay] = useState("none");
 
     const increment = 0.5;
     
+    
     useEffect(() => {
         const timer = setInterval(() => {        
         if(time < chat.length){
             console.log(subs)
-            setTime(time + increment);
-            if( Number.isInteger(time)){
-                if(chat[time]["sub"]) { 
-                    setSub(subs+1);
-                } 
-                if(subs >= subGoal){
-                    setMessageDisplay("flex");
-                }
-                else
-                    setMessageDisplay("none")
+            setTime(time + 1);
+            if(chat[time]["sub"]) { 
+                setSub(subs+1);
+            } 
+            if(subs >= subGoal){
+                setMessageDisplay("flex");
             }
+            else
+                setMessageDisplay("none")
+            
         }       
         }, 900);
         return () => clearInterval(timer);
